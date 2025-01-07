09fed7d9a8e017f426cebbfbe5113417504a68f8
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-06 20:14:47 -0500

    Add 2011_final_value_of_variable_after_performing_operations.py

---
+++ 1_Easy/2011_final_value_of_variable_after_performing_operations.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+
+        return sum(1 if "++" in op else -1 for op in operations)
+    
+print(Solution.finalValueAfterOperations(Solution, ["--X","X++","X++"]))
