a4ad80add353600c731496b58c6c3b94b599666f
Oscar Guerrero <guerreroriveraoscar@outlook.com>
2025-01-11 21:40:30 -0500

    add easy exercise 1470. Shuffle the Array

---
+++ 1_Easy/1470_shuffle_the_array.py
@@ -0,0 +1,22 @@
+"""
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Example 1:
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+"""
+
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        return [nums[i//2] if i % 2 == 0 
+        else nums[n + i // 2] 
+        for i in range(2*n)
+        ]
+    
